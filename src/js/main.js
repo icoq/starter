8085437e260866ddc6ae4b65e9b92bc3ee799046
Hike <hakosargsian@gmail.com>
2020-05-31 20:56:42 +0400

    rename fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-~include("../modules/header/header.js");
-~include("../modules/sidebar/sidebar.js");
-~include("../modules/footer/footer.js");
+~include("../modules/_header/header.js");
+~include("../modules/_sidebar/sidebar.js");
+~include("../modules/_footer/footer.js");
 ~include("./helper.js");
