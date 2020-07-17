4e2594406164a9246a574161f8c0706aa8793d24
Hike <hakosargsian@gmail.com>
2020-07-17 21:27:41 +0400

    changes

--- smart-grid-config.js
+++ smart-grid-config.js
@@ -6,25 +6,25 @@ var settings = {
   columns: 12 /* number of grid columns */,
   offset: "30px" /* gutter width px || % || rem */,
   mobileFirst: false /* mobileFirst ? 'min-width' : 'max-width' */,
-  container: {
-    maxWidth: "1200px" /* max-width оn very large screen */,
-    fields: "30px" /* side fields */
-  },
+  //   container: {
+  //     maxWidth: "1200px" /* max-width оn very large screen */,
+  //     fields: "30px" /* side fields */,
+  //   },
   breakPoints: {
     lg: {
-      width: "1100px" /* -> @media (max-width: 1100px) */
+      width: "1140px" /* -> @media (max-width: 1100px) */,
     },
     md: {
-      width: "960px"
+      width: "992px",
     },
     sm: {
-      width: "780px",
+      width: "768px",
       fields:
-        "15px" /* set fields only if you want to change container.fields */
+        "15px" /* set fields only if you want to change container.fields */,
     },
     xs: {
-      width: "560px"
-    }
+      width: "480px",
+    },
     /* 
         We can create any quantity of break points.
 
@@ -34,7 +34,7 @@ var settings = {
             offset: 'N(px|%|rem)'
         }
         */
-  }
+  },
 };
 
 smartgrid("./", settings);
